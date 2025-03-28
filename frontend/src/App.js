d8ab694c7d99a04e7971f40b8f3383f27ee2b558
Bhishman <bhishman.desai@nshealth.ca>
2025-03-28 13:22:09 -0300

    feat: Adding initial project structure with React components and configuration files

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">iOS Style Memoji Generator</h1>
+        <button className="App-button">Generate</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
